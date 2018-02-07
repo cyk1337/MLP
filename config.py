b3c5d1d47f413e492a1aec972655ee1126d8ba1e
Punchwes <pengqiwei.pqw@gmail.com>
2018-02-07 22:08:57 +0000

    Signed-off-by: Punchwes <pengqiwei.pqw@gmail.com>

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ Imdb_file = os.path.join(work_dir,'dataset/aclImdb/')
 # data file
 train_pos = os.path.join( Imdb_file, 'train/pos')
 train_neg = os.path.join(Imdb_file, 'train/neg')
-test_pos = os.path.join(Imdb_file, 'train/pos')
-test_neg = os.path.join(work_dir, Imdb_file, 'train/neg')
+test_pos = os.path.join(Imdb_file, 'test/pos')
+test_neg = os.path.join(work_dir, Imdb_file, 'test/neg')
