8ce2ba95a3f47e49ac7e9701ed219c81efce43bd
Punchwes <pengqiwei.pqw@gmail.com>
2018-02-11 20:13:01 +0000

    edited path

--- pengqw/imdb.py
+++ pengqw/imdb.py
@@ -4,10 +4,9 @@ Created on Wed Feb  7 19:49:20 2018
 
 @author: pqw1995@163.com
 """
-
-#from config.setting import *
-
-#import __init__
+import sys
+sys.path.append('D:\\MLP_Project\\MLP')
+from config.setting import *
 import json
 import os
 from keras.preprocessing.text import Tokenizer
@@ -65,20 +64,20 @@ from keras.preprocessing.text import text_to_word_sequence
 #\d+|[\u4e00-\u9fff]+
 
 
-train_data=pd.read_csv('train_data.csv')
-
-train_data['text'] = train_data.apply(lambda row: text_to_word_sequence(row['text']), axis=1)
-#for inde, x in enumerate(train_data['text']):
-##    mm= [w.lower() for w in x]
-#    mm= [word for word in x if word.isalpha()]
-#    train_data['text'][inde]=mm
-
-print ('the preprocessing is done')
-vec_model = Word2Vec(train_data['text'],size=100, window=5, min_count=5, workers=multiprocessing.cpu_count()*2, sg=0, iter=40,compute_loss=True)
-print ('vector model training process is done') 
-print ('vocabulary size is :', len(vec_model.wv.index2word))
-print ('the latese loss is :', vec_model.get_latest_training_loss())
-#vec_model.save('vec_model_sg')
-vec_model.wv.save_word2vec_format('100d_vec_model_sg0.txt',binary=False)
+#train_data=pd.read_csv(train_csv)
+#
+#train_data['text'] = train_data.apply(lambda row: text_to_word_sequence(row['text']), axis=1)
+##for inde, x in enumerate(train_data['text']):
+###    mm= [w.lower() for w in x]
+##    mm= [word for word in x if word.isalpha()]
+##    train_data['text'][inde]=mm
+#
+#print ('the preprocessing is done')
+#vec_model = Word2Vec(train_data['text'],size=100, window=5, min_count=5, workers=multiprocessing.cpu_count()*2, sg=0, iter=40,compute_loss=True)
+#print ('vector model training process is done') 
+#print ('vocabulary size is :', len(vec_model.wv.index2word))
+#print ('the latese loss is :', vec_model.get_latest_training_loss())
+##vec_model.save('vec_model_sg')
+#vec_model.wv.save_word2vec_format('100d_vec_model_sg0.txt',binary=False)
 
     
