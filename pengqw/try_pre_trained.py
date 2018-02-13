15402c850a6748a09fa67d66b03dcababfaa8ad7
Punchwes <pengqiwei.pqw@gmail.com>
2018-02-13 22:09:14 +0000

    dddd

--- pengqw/try_pre_trained.py
+++ pengqw/try_pre_trained.py
@@ -147,7 +147,7 @@ model.add(Conv1D(100,
                  strides=1))
 model.add(GlobalMaxPooling1D())
 print ('after maxpooling layer the shape is:',model.output_shape)
-model.add(Dense(100,activation='relu'))
+model.add(Dense(150,activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1,activation='sigmoid'))
 
@@ -183,16 +183,18 @@ print("Loss: %.2f,  Accuracy: %.2f%%" % (scores[0],scores[1]*100))
 print (history.history.keys())
 
 
-write_filename='model_CBOW_CNN_dropout_05_size5_100unit.pdf'
-save_history(history, 'CBOW_CNN_dropout05_size5_100unit.csv', subdir='CBOW_CNN_MODEL')
+write_filename='model_CBOW_CNN_dropout_05_size5_200unit.pdf'
+save_history(history, 'CBOW_CNN_dropout05_size5_200unit.csv', subdir='CBOW_CNN_MODEL')
 visialize_model(model, write_filename)
 plot_fit(history, plot_filename=write_filename)
 
 
 
 
-##### 
-##### model_glove_CNN_dropout_05:  best: val_loss:0.2881; val_acc:0.8778
+##### CBOW_CNN_dropout05_size5_100unit: val_loss 0.2674; val_acc 0.8890 
+##### CBOW_CNN_dropout05_size5_150unit: val_loss 0.2675; val_acc 0.8898
+##### CBOW_CNN_dropout05_size5_200unit: val_loss 0.2711; val_acc 0.8852
+
 
 
 
