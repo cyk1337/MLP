43e7eb655ad0ac6c9509b0dcba4a300a091e3b30
Punchwes <pengqiwei.pqw@gmail.com>
2018-02-14 20:34:21 +0000

    dddd

--- pengqw/try_pre_trained.py
+++ pengqw/try_pre_trained.py
@@ -147,6 +147,13 @@ model.add(Conv1D(100,
                  strides=1))
 model.add(GlobalMaxPooling1D())
 
+model.add(Conv1D(100,
+                 4,
+                 padding='valid',
+                 activation='relu',
+                 strides=1))
+model.add(GlobalMaxPooling1D())
+
 print ('after maxpooling layer the shape is:',model.output_shape)
 model.add(Dense(150,activation='relu'))
 model.add(Dropout(0.5))
@@ -184,8 +191,8 @@ print("Loss: %.2f,  Accuracy: %.2f%%" % (scores[0],scores[1]*100))
 print (history.history.keys())
 
 
-write_filename='model_CBOW_CNN_dropout_02_size5.pdf'
-save_history(history, 'CBOW_CNN_dropout02_size5.csv', subdir='CBOW_CNN_dropout02_kernel_X_size')
+write_filename='model_CBOW_2CNN_dropout_02_size5.pdf'
+save_history(history, 'CBOW_2CNN_dropout02_size5.csv', subdir='CBOW_CNN_dropout02_kernel_X_size')
 visialize_model(model, write_filename)
 plot_fit(history, plot_filename=write_filename)
 
