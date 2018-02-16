d7f549db68707b8deebce33dfc89347e8e1e4fa5
Punchwes <pengqiwei.pqw@gmail.com>
2018-02-16 21:49:04 +0000

    CNN_different embedding

--- pengqw/plot__all.py
+++ pengqw/plot__all.py
@@ -9,7 +9,7 @@ sys.path.append('D:\\MLP_Project\\MLP')
 from config.setting import *
 from CYK.plot_fit import *
 
-subdir = 'CBOW_CNN_kernelsize4_dropout_X'
-plot_all_history(subdir, plot_filename='CBOW_CNN_kernelsize4_dropout_X.pdf',figsize=(16,8))
+subdir = 'FixedCNN_Different_Embedding'
+plot_all_history(subdir, plot_filename='FixedCNN_Different_Embedding.pdf',figsize=(16,8))
 
 
