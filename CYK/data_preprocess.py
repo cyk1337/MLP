e430f16e89161d7432d89f11ea008853b4020b77
whisperer0 <chaiyekun@yahoo.com>
2018-02-18 16:50:27 +0000

    split data

--- CYK/data_preprocess.py
+++ CYK/data_preprocess.py
@@ -80,6 +80,55 @@ def generate_csv(folders, csv_file, shuffle_seed=SEED):
     else:
         print('%s already exists!' % csv_file)
 
+
+
+def split_data(folders, shuffle_seed=SEED):
+    # hand label then shuffle
+    # =============================
+    # num_neg = get_file_num(train_neg)
+    # num_pos = get_file_num(train_pos)
+    # # label
+    # labels = np.concatenate((np.zeros(num_neg), np.ones(num_pos)))
+    # =============================
+    records = []
+    for folder in folders:
+        record = get_doc_list(folder)
+        records.extend(record)
+    dataset = pd.DataFrame.from_records(records, columns=['text', 'star'])
+    dataset['target'] = 0
+    dataset.loc[dataset['star'].astype('int64') >= 7, 'target'] = 1
+
+    shuffle_data = dataset.sample(frac=1, random_state=shuffle_seed)
+
+    # split
+    split_bound = [0.70, 0.15, 0.15]
+    RECORDS_NUM = len(shuffle_data)
+    train_bound = int(split_bound[0]* RECORDS_NUM)
+    val_bound = int((split_bound[0] + split_bound[1]) * RECORDS_NUM)
+    train_data = shuffle_data.iloc[:train_bound, :]
+    val_data = shuffle_data.iloc[train_bound:val_bound, :]
+    test_data = shuffle_data.iloc[val_bound:, :]
+
+
+
+    if not os.path.exists(data_file):
+        os.mkdir(data_file)
+    if not os.path.exists(train_csv):
+        print('Generating {}...'.format(train_csv))
+        train_data.to_csv(train_csv, index=False, encoding='utf-8')
+        print('Finished', '-'*80)
+    if not os.path.exists(val_csv):
+        print('Generating {}...'.format(val_csv))
+        val_data.to_csv(val_csv, index=False, encoding='utf-8')
+        print('Finished','-'*80)
+    if not os.path.exists(test_csv):
+        print('Generating {}...'.format(test_csv))
+        test_data.to_csv(test_csv, index=False, encoding='utf-8')
+        print('Finished','-'*80)
+    else:
+        print('%s, %s , %s already exists!' % (train_csv, val_csv, test_csv))
+
+
 def del_csv(path):
     if os.path.exists(path):
         shutil.rmtree(path)
@@ -91,16 +140,22 @@ if __name__=="__main__":
     test_folders = [test_neg, test_pos]
 
     # del file and regenerate
-    del_csv(data_file)
-
+    if os.path.exists(data_file):
+        del_csv(data_file)
+    """   
     # generate train and test csv file if not exist
     if not os.path.exists(data_file):
         os.mkdir(data_file)
+    # generate training set
     if not os.path.exists(train_csv):
         generate_csv(train_folders, csv_file=train_csv)
-    if not os.path.exists(test_csv):
-        generate_csv(test_folders, csv_file=test_csv)
-    print('Train and test data generated!')
+    print('Training set generated!')
+    """
+
+    # split test and validation set
+    if not os.path.exists(test_csv) and not os.path.exists(val_csv):
+        split_data(folders=[train_folders, test_folders])
+    print('Validation and test set generated!')
     print('-'*80)
 
 
