9c2c10fb52697a13a90417a0543f0c5feb720f97
whisperer0 <chaiyekun@yahoo.com>
2018-03-28 21:45:11 +0100

    mlp

--- CYK/embedding_visual_comparision.py
+++ CYK/embedding_visual_comparision.py
@@ -34,7 +34,8 @@ import matplotlib.pyplot as plt
 embeddings = ['cbow', 'skipgram', 'glove']
 embeddings_path = [CBOW_embedding, SkipGram_embedding, Glove_embedding]
 # embedding index
-embedding_num = 2 # glove
+# embedding_num = 2 # glove
+embedding_num = 1 # CBOW
 embedding_path = os.path.join(embedding_dir, embeddings_path[embedding_num])
 embeddings_index = load_pretrained_model(embedding_path)
 
@@ -93,10 +94,11 @@ for i, label in enumerate(labels2):
                  fontsize=20,
                  ha='right',
                  va='bottom')
-plt.title('GloVe embedding', fontsize=20)
+# plt.title('GloVe embedding', fontsize=20)
+plt.title('Skipgram embedding', fontsize=20)
 
 
-savepath = os.path.join(embedding_dir, 'CBOW_GloVe_comparison.pdf')
+savepath = os.path.join(embedding_dir, 'CBOW_Skipgram_comparison.pdf')
 plt.savefig(savepath)
 print('Saved to',savepath)
 plt.show()
